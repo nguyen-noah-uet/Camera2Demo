c062656d6d02c087c0c68ccc50fef4f6fac8126a
nguyen-noah-uet <20020666@vnu.edu.vn>
2023-11-14 22:21:37 +0700

    Update change focus distance

--- app/src/main/java/com/example/camera2demo/MainActivity.kt
+++ app/src/main/java/com/example/camera2demo/MainActivity.kt
@@ -3,47 +3,68 @@ package com.example.camera2demo
 import android.Manifest.permission
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.graphics.ImageFormat
+import android.graphics.Bitmap
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
 import android.media.ImageReader
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
+import android.util.Log
+import android.view.MotionEvent
 import android.view.Surface
 import android.view.TextureView
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.app.ActivityCompat
+import com.google.android.material.slider.Slider
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    private val TAG = "CAMERA2DEMO_MainActivity"
+    private var isAF = false
+    private var isAWB = false
     private lateinit var captureBtn: Button
-    lateinit var cameraManager: CameraManager
-    lateinit var textureView: TextureView
-    lateinit var cameraCaptureSession: CameraCaptureSession
-    lateinit var cameraDevice: CameraDevice
-    lateinit var captureRequestBuilder : CaptureRequest.Builder
-    lateinit var captureRequest: CaptureRequest
-    lateinit var handler: Handler
-    lateinit var handlerThread: HandlerThread
-    lateinit var imageReader: ImageReader
+    private lateinit var cameraManager: CameraManager
+    private lateinit var textureView: TextureView
+    private lateinit var cameraCaptureSession: CameraCaptureSession
+    private lateinit var cameraDevice: CameraDevice
+    private lateinit var captureRequestBuilder : CaptureRequest.Builder
+    private lateinit var captureRequest: CaptureRequest
+    private lateinit var handler: Handler
+    private lateinit var handlerThread: HandlerThread
+    private lateinit var imageReader: ImageReader
+    private lateinit var focusDistanceSlider: Slider
+    private lateinit var autoFocusModeSwitch: SwitchCompat
+    private lateinit var autoWBMode: SwitchCompat
+    private lateinit var bitmap: Bitmap
+
     val context = this
+
+
+    private fun setFocusDistance(distance: Float) {
+        captureRequestBuilder.set(CaptureRequest.LENS_FOCUS_DISTANCE, distance)
+        cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
+    }
+    private fun getFocusDistance(): Float? {
+        return captureRequestBuilder.get(CaptureRequest.LENS_FOCUS_DISTANCE)
+    }
+    private fun setFocusMode(mode: Int) {
+        captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, mode)
+        cameraCaptureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
             setContentView(R.layout.activity_main)
-
+            bindControls()
+            wireEvents()
             getPermission()
-
-
-            textureView = findViewById(R.id.textureView)
             cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
 
             handlerThread = HandlerThread("Camera2")
@@ -64,18 +85,64 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
-
+                    bitmap = textureView.bitmap!!
+                    var focusDistance = getFocusDistance()!!
+                    Log.d(TAG, "focus distance: $focusDistance")
                 }
             }
-
-
-            captureBtn = findViewById<Button>(R.id.captureBtn)
         } catch (e: Exception) {
             e.printStackTrace()
         }
-
-
     }
+
+    private fun bindControls() {
+        textureView = findViewById(R.id.textureView)
+        captureBtn = findViewById<Button>(R.id.captureBtn)
+        focusDistanceSlider = findViewById<Slider>(R.id.focusDistanceSlider)
+        autoFocusModeSwitch = findViewById<SwitchCompat>(R.id.autoFocusModeSwitch)
+        autoWBMode = findViewById<SwitchCompat>(R.id.autoWBMode)
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    private fun wireEvents() {
+
+        autoFocusModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            isAF = isChecked
+            if (isAF) {
+                setFocusMode(CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+
+                // disable focus distance slider
+                focusDistanceSlider.isEnabled = false
+            }
+            else {
+                setFocusMode(CaptureRequest.CONTROL_AF_MODE_OFF)
+                // enable focus distance slider
+                focusDistanceSlider.isEnabled = true
+            }
+        }
+
+        focusDistanceSlider.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {
+            }
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                if(!isAF)
+                    setFocusDistance(slider.value)
+            }
+        })
+
+        textureView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                try {
+                    // handleTouchToFocus(event.x, event.y)
+                    return@setOnTouchListener true
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            return@setOnTouchListener false
+        }
+    }
+
 
     private fun open_camera() {
         if (ActivityCompat.checkSelfPermission(this, permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -99,27 +166,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun takePreview() {
         val surfaceTexture = textureView.surfaceTexture
-//        surfaceTexture?.setDefaultBufferSize(textureView.width, textureView.height)
         val surface = Surface(surfaceTexture)
+        surfaceTexture?.setDefaultBufferSize(textureView.width, textureView.height)
+
         captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
         captureRequestBuilder.addTarget(surface)
         cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
             override fun onConfigured(session: CameraCaptureSession) {
                 cameraCaptureSession = session
-                captureRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_OFF)
-                captureRequestBuilder.set(CaptureRequest.LENS_FOCUS_DISTANCE, 6.0f)
+                // turn off autofocus
+                setFocusMode(CaptureRequest.CONTROL_AF_MODE_OFF)
+
                 captureRequest = captureRequestBuilder.build()
-                cameraCaptureSession.setRepeatingRequest(captureRequest, null, handler)
+                cameraCaptureSession.setRepeatingRequest(captureRequest, null, null)
             }
 
             override fun onConfigureFailed(session: CameraCaptureSession) {
                 Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show()
             }
         }, handler)
+
     }
 
     private fun getPermission() {
-        var permissionList = mutableListOf<String>()
+        val permissionList = mutableListOf<String>()
         if (checkSelfPermission(permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             permissionList.add(permission.CAMERA)
         }
